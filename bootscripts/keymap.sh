c943d318f302c223b6eced2af4f33165c1625d47
ecocomputing <peregrinebrian@gmail.com>
2018-02-14 10:02:52 +0100

    Update keymap.sh

--- bootscripts/keymap.sh
+++ bootscripts/keymap.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# Commands in the /bootscripts folder have priority over the configuration commands placed in /etc/local.d
+# So, altering these will change your the settings loaded by the TAZ livecd/usb
+
 setxkbmap -layout be
