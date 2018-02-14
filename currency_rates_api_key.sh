f2fec6c8ea92fb023364b05290c57182f93a856d
ecocomputing <peregrinebrian@gmail.com>
2018-02-14 10:24:19 +0100

    Create currency_rates_api_key.sh

---
+++ currency_rates_api_key.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Place your api key in this file; it will then get read by the currency rates api provider to attain the currency info for display 
+# on conky. At the moment of writing, neither fixer.io (regular currency), nor binance 
+# (https://api.binance.com/api/v1/ticker/allPrices ) requires an api key so this file is not yet needed. 
+# Could be needed in the future, so that's why this file was made. Gold and silver (xaueur , xageur) rates are not yet provided.
+#
+
