c339f21e036cf85330bd826dd8a45d38662beb66
ecocomputing <peregrinebrian@gmail.com>
2018-02-20 10:05:03 +0100

    Update and rename weather_underground_api_key.sh to weather_api_key.sh

---
+++ api_keys/weather_api_key.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Place your weather underground api key in this file; it will then get read by weather underground to attain the weather info for display 
+#on conky. See also: http://help.wunderground.com/knowledgebase/articles/656767-where-is-my-api-key
+
--- api_keys/weather_underground_api_key.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Place your api key in this file; it will then get read by weather underground to attain the weather info for display 
-#on conky. See also: http://help.wunderground.com/knowledgebase/articles/656767-where-is-my-api-key
-
