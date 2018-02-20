1fd6060bc78e9ed83661873d0cc297d5f3c9fccd
ecocomputing <peregrinebrian@gmail.com>
2018-02-20 09:54:09 +0100

    Add files via upload

---
+++ conky_files/conkyscripts/coinapi_io.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#
+# Fetches currency rates info from coinapi.io
+
+#Program code:
+#retrieve key from currency_rates_api_key.sh
+
---
+++ conky_files/conkyscripts/fixer_io_script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#
+# Fetches currency rates info from fixer.io
+# no api key needed, if a key becomes needed later on, retrieve key from currency_rates_api_key.sh on
+# optional USB stick
+
+
+url: http://api.fixer.io/
