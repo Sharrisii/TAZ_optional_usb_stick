7659018058a9bf73ef4b162624912859621a9225
ecocomputing <peregrinebrian@gmail.com>
2018-02-22 15:32:41 +0100

    Update keymap.sh

--- keymaps/keymap.sh
+++ keymaps/keymap.sh
@@ -1,14 +1,6 @@
 # this file overrides the 30-keyboard.conf file located in /etc/X11/xorg.conf.d; if the usb stick is not inserted 
 # (and this file is hence unavailable, the keymap in /etc/X11/xorg.conf.d/30-keyboard.conf will be used instead.
 # see https://wiki.gentoo.org/wiki/Xorg/Guide#Configuring_the_keyboard
+# and https://wiki.gentoo.org/wiki/Keyboard_layout_switching#Using_setxkbmap
 
-Section "InputClass"
-  Identifier "keyboard-all"
-  Driver "evdev"
-  Option "XkbLayout" "be"
-  Option "XkbModel" "pc101"
-  Option "XkbRules" "xorg"
-  Option "XkbOptions" ""
-  Option "XkbVariant" ""
-  MatchIsKeyboard "on"
-EndSection
+setxkbmap -layout be
