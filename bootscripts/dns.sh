7e959cdd4e291d92206718e33dd7595e0cf3a09a
ecocomputing <peregrinebrian@gmail.com>
2018-02-27 11:09:29 +0100

    Update dns.sh

--- bootscripts/dns.sh
+++ bootscripts/dns.sh
@@ -1,4 +1,4 @@
-# this file overrides the resolve.conf file located in /etc/; if the usb stick is not inserted
-# (and this file is hence unavailable, the dns in /etc/resolve.conf will be used instead.
+# this file overrides the resolv.conf file located in /etc/; if the usb stick is not inserted
+# (and this file is hence unavailable, the dns in /etc/resolv.conf will be used instead.
 
 command ?
