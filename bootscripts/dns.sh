21ae6d81e1e2440e00c179c4f45a9b7832c9f45b
ecocomputing <peregrinebrian@gmail.com>
2018-02-27 11:24:23 +0100

    Update dns.sh

--- bootscripts/dns.sh
+++ bootscripts/dns.sh
@@ -1,4 +1,6 @@
-# this file overrides the resolv.conf file located in /etc/; if the usb stick is not inserted
+# this file modifies the resolv.conf file located in /etc/; if the usb stick is not inserted
 # (and this file is hence unavailable, the dns in /etc/resolv.conf will be used instead.
 
-command ?
++chattr -i /etc/resolv.conf
+sed -i 's/???.???.?.?/input_your_new_IP_here/g' resolv.conf
++chattr +i /etc/resolv.conf
