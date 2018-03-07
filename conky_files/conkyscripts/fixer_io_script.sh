cb36c163454a7d8553e224129f2e2b19448103ba
ecocomputing <peregrinebrian@gmail.com>
2018-03-07 14:17:55 +0100

    Update fixer_io_script.sh

--- conky_files/conkyscripts/fixer_io_script.sh
+++ conky_files/conkyscripts/fixer_io_script.sh
@@ -3,9 +3,7 @@
 # Retrieves and displays currency rates info from fixer.io
 # Script made by Brian Peregrine, under GPL v3 license.
 #
-# no api key needed, if a key becomes needed later on, retrieve key from currency_rates_api_key.sh on
-# optional USB stick and store under [API_key_fixer] to be called upon in the HTTP requests
-#
+# no api key needed, if a key becomes needed later on, it can be added later-on
 #
 # Program code:
 import urllib2
