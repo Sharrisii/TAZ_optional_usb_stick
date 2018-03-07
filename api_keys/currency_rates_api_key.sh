21d51ae1fddedacacd1977bd9f9cebbc008f99be
ecocomputing <peregrinebrian@gmail.com>
2018-03-07 14:24:05 +0100

    Delete currency_rates_api_key.sh

--- api_keys/currency_rates_api_key.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-#
-# Place your fixer.io api key in this file; it will then get read by the fixer.io to attain the currency info for display 
-# on conky. At the moment of writing, fixer.io does not yet require an api key so this file is not yet needed. 
-# Could be needed in the future, so that's why this file was made. Gold and silver (xaueur , xageur) rates are not yet provided by fixer.io.
