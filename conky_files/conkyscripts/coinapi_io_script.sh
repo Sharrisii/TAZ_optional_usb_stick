7aa6ba5ebde09d317620b09177fbea09a9ef12c8
ecocomputing <peregrinebrian@gmail.com>
2018-03-07 14:28:22 +0100

    Update coinapi_io_script.sh

--- conky_files/conkyscripts/coinapi_io_script.sh
+++ conky_files/conkyscripts/coinapi_io_script.sh
@@ -14,32 +14,32 @@ API_key_coinapi = "insert_API_key_here"
 
 # Define additional parameters
 #timestamp format example: 2018-02-23T15:45:56.0000000Z
-[timestamp_today]] = [clock.year]-[clock.month]-[clock.day]T[clock.hour]:[clock.minute]:[clock.second].0000000Z
-[timestamp_yesterday] = [clock.year]-[clock.month]-([clock.day]-1)T[clock.hour]:[clock.minute]:[clock.second].0000000Z
+from datetime import datetime, timedelta
+clock = datetime.utcnow()
+timestamp_today =     str(str(clock.year) + ('-' if len(str(clock.month)) > 1 else '-0') + str(clock.month) + ('-' if len(str(clock.day)) > 1 else '-0') + str(clock.day)) + 'T' + ('0' if len(str(clock.hour)) < 2 else '') + str(clock.hour) + ':' + ('0' if len(str(clock.minute)) < 2 else '') + str(clock.minute) + ':' + ('0' if len(str(clock.second)) < 2 else '') + str(clock.second) + 'Z'
+clock = datetime.utcnow() - timedelta(days=1)
+timestamp_yesterday = str(str(clock.year) + ('-' if len(str(clock.month)) > 1 else '-0') + str(clock.month) + ('-' if len(str(clock.day)) > 1 else '-0') + str(clock.day)) + 'T' + ('0' if len(str(clock.hour)) < 2 else '') + str(clock.hour) + ':' + ('0' if len(str(clock.minute)) < 2 else '') + str(clock.minute) + ':' + ('0' if len(str(clock.second)) < 2 else '') + str(clock.second) + 'Z'
 
-# Do the "current exchangerate" info request for Bitcoin (XBT), with the imported API key, and store the output data 
-# temporarily under the name "[currency_rate_today_xbteur]"
-import urllib2
-import json
-f = urllib2.urlopen(https://rest.coinapi.io/v1/exchangerate/XBT/EUR?apikey=[API_key_coinapi]?time=[timestamp_today]') 
-parsed_json = json.loads(json_string) 
-output value recall (echo?)= [currency_rate_today_xbteur]
+# Do the "current exchangerate" info request for Bitcoin (BTC), with the imported API key, and store the output data 
+# temporarily under the name "currency_rate_today_btceur"
+f = urllib2.urlopen('https://rest.coinapi.io/v1/exchangerate/BTC/EUR?apikey=' + API_key_coinapi + '&time=' + timestamp_today)
+json_string = f.read()
+parsed_json = json.loads(json_string)
+currency_rate_today_btceur = parsed_json['rate']
 
 # Do the "historical exchangerate" info request for Bitcoin (XBT), with the imported API key, and store the output data 
-# temporarily under the name "[currency_rate_yesterday_xbteur]"
-import urllib2
-import json
-f = urllib2.urlopen(https://rest.coinapi.io/v1/exchangerate/XBT/EUR?apikey=[API_key_coinapi]?time=[timestamp_yesterday]') 
-parsed_json = json.loads(json_string) 
-output value recall (echo?)= [currency_rate_yesterday_xbteur]
+# temporarily under the name "currency_rate_yesterday_btceur"
+f = urllib2.urlopen('https://rest.coinapi.io/v1/exchangerate/BTC/EUR?apikey=' + API_key_coinapi + '&time=' + timestamp_yesterday)
+json_string = f.read().replace("'",'"').replace('u"','"')
+parsed_json = json.loads(json_string)
+currency_rate_yesterday_btceur = parsed_json['rate']
                     
 # Calculate the increase of the currency rate of today compared to yesterday's rate and store the output data temporarily 
-# under the name "[xbteur_rate_increase_percent_compared_to_yesterdays_rate]"
-[xbteur_rate_increase_percent_compared_to_yesterdays_rate] = ( [currency_rate_today_xbteur] - [currency_rate_yesterday_xbteur] ) / [currency_rate_yesterday_xbteur]
+# under the name "[btceur_rate_increase_percent_compared_to_yesterdays_rate]"
+btceur_rate_increase_percent_compared_to_yesterdays_rate = ( currency_rate_today_btceur - currency_rate_yesterday_btceur ) / currency_rate_yesterday_btceur
 
 # Display the info
-print "XBTEUR: [currency_rate_today_xbteur] ([xbteur_rate_increase_percent_compared_to_yesterdays_rate] %)"
-parsed_json ?, parsed_json ?)
+print "BTCEUR: " + str(currency_rate_today_btceur) + "(" + str(btceur_rate_increase_percent_compared_to_yesterdays_rate) + "%)"
 f.close()
 
 Do the same requests for:
