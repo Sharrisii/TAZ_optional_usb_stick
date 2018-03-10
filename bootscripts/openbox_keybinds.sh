7ae19a5163b51e5019e65c74e2b15323b56c8c51
ecocomputing <peregrinebrian@gmail.com>
2018-03-10 11:48:41 +0100

    Update openbox_keybinds.sh

--- bootscripts/openbox_keybinds.sh
+++ bootscripts/openbox_keybinds.sh
@@ -1,6 +1,9 @@
 
 # this file gives the command to replace the menu.xml and rc.xml; if the usb stick is not inserted 
-# (and these file is hence unavailable, the standard menu.xml and rc.xml in /etc/xdg/openbox/ will be used instead.
+# (and these file is hence unavailable, the standard menu.xml and rc.xml in /etc/xdg/openbox/ will be used instead
+# once the files have been replaced, a command has been added to restart openbox with the new config files
+# see https://linux.die.net/man/1/openbox
 
-cp ./openbox_configs/menu.xml /etc/xdg/openbox/menu.xml
-cp ./openbox_configs/menu.xml /etc/xdg/openbox/rc.xml
+cp -f ./openbox_configs/menu.xml /etc/xdg/openbox/menu.xml
+cp -f ./openbox_configs/menu.xml /etc/xdg/openbox/rc.xml
+openbox --reconfigure
