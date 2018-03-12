845bb38fad6b4371bc4157cb8a2a41d962118947
ecocomputing <peregrinebrian@gmail.com>
2018-03-12 17:10:35 +0100

    Update printer_setup.sh

--- bootscripts/printer_setup.sh
+++ bootscripts/printer_setup.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+# This file tells the system to copy the ppd file, and restart cups
+# see https://wiki.gentoo.org/wiki/Printing and https://forums.gentoo.org/viewtopic-t-22291-start-0.html
 
+cp ../printer_files/*.ppd /usr/share/cups/model/
+/etc/init.d/cupsd restart
