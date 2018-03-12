4b146f854b3ce46e856251d6e15240db40341ecf
ecocomputing <peregrinebrian@gmail.com>
2018-03-12 17:42:27 +0100

    Update printer_setup.sh

--- bootscripts/printer_setup.sh
+++ bootscripts/printer_setup.sh
@@ -6,6 +6,7 @@
 # Next, set up cups by pointing the browser to http://localhost:631 and following the instructions
 # cups will then generate a ? file. Place it in /printer_files/
 # Then, get your .ppd file for your printer from https://www.openprinting.org/printers and place it in /printer_files/
+# Once you generated both files, you won't need to go over these steps any more (the script will do the rest from then on).
 # This script will then copy the ppd file to the system, and restart cups, and start the cups deamon
 # see https://wiki.gentoo.org/wiki/Printing and https://forums.gentoo.org/viewtopic-t-22291-start-0.html
 
