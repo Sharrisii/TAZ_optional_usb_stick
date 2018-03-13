c18300a3842bef9d4fe927f6e9834ac4fb65ef18
ecocomputing <peregrinebrian@gmail.com>
2018-03-13 13:33:10 +0100

    Update quandl_script.sh

--- conky_files/conkyscripts/quandl_script.sh
+++ conky_files/conkyscripts/quandl_script.sh
@@ -3,7 +3,7 @@
 # Retrieves and displays comodity rates info from quandl.com
 # Script made by Brian Peregrine, under GPL v3 license.
 # Docs for quandl are at https://docs.quandl.com/docs/
-# For a list of what comodity rates are available see https://blog.quandl.com/api-for-commodity-data
+# For a list of what commodity rates are available see https://blog.quandl.com/api-for-commodity-data
 
 # Get your API key via https://www.quandl.com/?modal=register
 # Then, place your API key in this script. The program will do the rest.
@@ -24,14 +24,14 @@ timestamp_yesterday = str(str(clock.year) + ('-' if len(str(clock.month)) > 1 el
 #------------------------------------------ Gold ------------------------------------------
 # Do the "current exchangerate" info request for gold (WORLDBANK/WLD_GOLD), with the imported API key, and store the output data 
 # temporarily under the name "currency_rate_today_xaueur"
-f = urllib2.urlopen('http://www.quandl.com/api/v3/datasets/WORLDBANK/WLD_PHOSROCK?apikey=' + API_key_quandl + '&time=' + timestamp_today)
+f = urllib2.urlopen('http://www.quandl.com/api/v3/datasets/WORLDBANK/WLD_GOLD?apikey=' + API_key_quandl + '&time=' + timestamp_today)
 json_string = f.read()
 parsed_json = json.loads(json_string)
 currency_rate_today_xaueur = parsed_json['rate']
 
 # Do the "historical exchangerate" info request for gold (WORLDBANK/WLD_GOLD), with the imported API key, and store the output data 
 # temporarily under the name "currency_rate_yesterday_xaueur"
-f = urllib2.urlopen('https://rest.coinapi.io/v1/exchangerate/BTC/EUR?apikey=' + API_key_quandl + '&time=' + timestamp_yesterday)
+f = urllib2.urlopen('http://www.quandl.com/api/v3/datasets/WORLDBANK/WLD_GOLD?apikey=' + API_key_quandl + '&time=' + timestamp_yesterday)
 json_string = f.read()
 parsed_json = json.loads(json_string)
 currency_rate_yesterday_xaueur = parsed_json['rate']
@@ -47,14 +47,14 @@ f.close()
 #------------------------------------------ Silver ------------------------------------------
 # Do the "current exchangerate" info request for silver (WORLDBANK/WLD_SILVER), with the imported API key, and store the output data 
 # temporarily under the name "currency_rate_today_xageur"
-f = urllib2.urlopen('https://rest.coinapi.io/v1/exchangerate/LTC/EUR?apikey=' + API_key_quandl + '&time=' + timestamp_today)
+f = urllib2.urlopen('http://www.quandl.com/api/v3/datasets/WORLDBANK/WLD_SILVER?apikey' + API_key_quandl + '&time=' + timestamp_today)
 json_string = f.read()
 parsed_json = json.loads(json_string)
 currency_rate_today_xageur = parsed_json['rate']
 
 # Do the "historical exchangerate" info request for silver (WORLDBANK/WLD_SILVER), with the imported API key, and store the output data 
 # temporarily under the name "currency_rate_yesterday_xageur"
-f = urllib2.urlopen('https://rest.coinapi.io/v1/exchangerate/LTC/EUR?apikey=' + API_key_quandl + '&time=' + timestamp_yesterday)
+f = urllib2.urlopen('http://www.quandl.com/api/v3/datasets/WORLDBANK/WLD_SILVER?apikey' + API_key_quandl + '&time=' + timestamp_yesterday)
 json_string = f.read()
 parsed_json = json.loads(json_string)
 currency_rate_yesterday_xageur = parsed_json['rate']
