86bff9d82ba99e2466344bce14864af084d47747
ecocomputing <peregrinebrian@gmail.com>
2018-03-14 16:24:47 +0100

    Update screenresolution.sh

--- bootscripts/screenresolution.sh
+++ bootscripts/screenresolution.sh
@@ -1,6 +1,7 @@
 # This file is triggered by the screen_res_lookup_on_usb_drives.start file in /etc/local.d/ in the TAZ distro (livecd/liveusb).
-# Commands in the /bootscripts folder have priority over the basic screen resolution
-# So, altering these will change your the settings loaded by the TAZ livecd/usb
+# The basic screen resolution is first used from /etc/X11/xorg.conf.d/, after this, the command below is carried out
+# which allow setting a different resolution
 # see https://wiki.gentoo.org/wiki/Xrandr
 
+sleep 2
 xrandr --output LVDS --mode 1024x768
