4e00ad7a35cd27beebb1172d163dd34b5b08cd4a
ecocomputing <peregrinebrian@gmail.com>
2018-03-14 10:03:02 +0100

    Update keymap.sh

--- bootscripts/keymap.sh
+++ bootscripts/keymap.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # this file overrides the 30-keyboard.conf file located in /etc/X11/xorg.conf.d; if the usb stick is not inserted 
 # (and this file is hence unavailable, the keymap in /etc/X11/xorg.conf.d/30-keyboard.conf will be used instead.
 # see https://wiki.gentoo.org/wiki/Xorg/Guide#Configuring_the_keyboard
