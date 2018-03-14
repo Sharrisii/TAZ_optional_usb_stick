bf986fccdaf752832d88ef4632cbae8ac82eef30
ecocomputing <peregrinebrian@gmail.com>
2018-03-14 09:46:42 +0100

    Update palemoon_secimp_settings.sh

--- bootscripts/palemoon_secimp_settings.sh
+++ bootscripts/palemoon_secimp_settings.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+# This file copies some settings, extensions and searchplugins to the palemoon folder (/opt/palemoon ) at bootup.
+# It also makes a /downloads/ folder under / and puts a Postinstallation.html in it.
 
+mkdir /downloads/
+cp ../palemoon_secimp_settings/downloads/Postinstallation.html /downloads/Postinstallation.html
+cp ../palemoon_secimp_settings/opt/palemoon/browser/extensions
