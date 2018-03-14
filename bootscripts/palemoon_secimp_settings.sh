3bdd34a9a7ea4d7807a00c2c0fe97b0709143c3d
ecocomputing <peregrinebrian@gmail.com>
2018-03-14 09:58:19 +0100

    Update palemoon_secimp_settings.sh

--- bootscripts/palemoon_secimp_settings.sh
+++ bootscripts/palemoon_secimp_settings.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 # This file copies some settings, extensions and searchplugins to the palemoon folder (/opt/palemoon ) at bootup.
-# It also makes a /downloads/ folder under / and puts a Postinstallation.html in it.
+# It will also delete the non-SSL searchplugins and make a /downloads/ folder under / and puts a Postinstallation.html in it.
+# See http://kb.mozillazine.org/Search_Bar
+# See http://kb.mozillazine.org/Installing_extensions
+# See https://mike.kaply.com/2012/03/15/customizing-firefox-default-preference-files/
 
 mkdir /downloads/
 cp ../palemoon_secimp_settings/downloads/Postinstallation.html /downloads/Postinstallation.html
-cp ../palemoon_secimp_settings/opt/palemoon/browser/extensions
+
+cd ../palemoon_secimp_settings/opt/
+cp Updated_about_config_settings.cfg /opt/palemoon
+
+cd ../palemoon_secimp_settings/opt/palemoon/defaults/pref/
+cp autoconfig.js /opt/palemoon/defaults/pref/
+
+cd ../palemoon_secimp_settings/opt/palemoon/browser/extensions
+cp  * /opt/palemoon/browser/extensions/
+
+cd /opt/palemoon/browser/searchplugins/
+rm *
+
+cd ../palemoon_secimp_settings/opt/palemoon/browser/searchplugins/
+cp * /opt/palemoon/browser/searchplugins/
