03de4b25373fdb2fa7d09e70ad236907264c58ac
Sharrisii <peregrinebrian@gmail.com>
2018-03-20 13:54:24 +0100

    Update change_device_names.sh

--- bootscripts/change_device_names.sh
+++ bootscripts/change_device_names.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-## ?? Create and delete symlinks ?
-## see https://forums.gentoo.org/viewtopic-p-8195222.html#8195222
+# Hide the /dev/cdrom, and /dev/dvd symlinks to sr0, as well as the unused and empty /dev/disk folder 
+# (normally used by systemd, but that's not installed)
+cp -f ../device_names_files/.hidden /dev/
+
+# Copy notes.txt to /dev/
+cp -f ../device_names_files/notes.txt /dev/
+
+# Copy udev rules file that makes udev automatically make labels for hard disks
+cp -f ../device_names_files/harddisk_labelling.rule /etc/udev/rules.d/
