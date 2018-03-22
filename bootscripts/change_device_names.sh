b97c44c84d1260aec68ca688641a49e577dfe2b4
Sharrisii <peregrinebrian@gmail.com>
2018-03-22 11:51:07 +0100

    Update change_device_names.sh

--- bootscripts/change_device_names.sh
+++ bootscripts/change_device_names.sh
@@ -7,12 +7,43 @@ cp -f ../device_names_files/.hidden /dev/
 # Copy notes.txt to /dev/
 cp -f ../device_names_files/notes.txt /dev/
 
-# Append udev rules to /lib/udev/rules.d/60-persistent-storage.rules tho make udev automatically make labels for hard disks
+# Append udev rules to /lib/udev/rules.d/60-persistent-storage.rules to make udev automatically make labels
+# for parallel, serial and usb hard drives, as well as parallel, serial and usb cd(r)(w), and dvd(r)(w) drives
 echo"
+KERNEL=="sd*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="disk",
+ENV{ID_ATA_SATA}!="1", SYMLINK+="disk/by-id/PARALLEL_ATA-$env{ID_PARALLEL}"
+KERNEL=="sd*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="partition",
+ENV{ID_SERIAL}=="?*",
+SYMLINK+="disk/by-id/PARALLEL_ATA-$env{ID_PARALLEL}-part%n"
+
 KERNEL=="sd*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="disk",
 ENV{ID_SERIAL}=="?*", SYMLINK+="disk/by-id/SERIAL_ATA-$env{ID_SERIAL}"
 KERNEL=="sd*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="partition",
 ENV{ID_SERIAL}=="?*",
 SYMLINK+="disk/by-id/SERIAL_ATA-$env{ID_SERIAL}-part%n"
+
+KERNEL=="sd*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="disk",
+ENV{ID_BUS}=="usb*", SYMLINK+="disk/by-id/USB-$env{ID_USB}"
+KERNEL=="sd*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="partition",
+ENV{ID_SERIAL}=="?*",
+SYMLINK+="disk/by-id/USB-$env{ID_USB}-part%n"
+
+KERNEL=="sr*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="disk",
+ENV{ID_ATA_SATA}!="1", SYMLINK+="disk/by-id/PARALLEL_ATA-$env{ID_PARALLEL}"
+KERNEL=="sd*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="partition",
+ENV{ID_SERIAL}=="?*",
+SYMLINK+="disk/by-id/PARALLEL_ATA-$env{ID_PARALLEL}-part%n"
+
+KERNEL=="sr*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="disk",
+ENV{ID_SERIAL}=="?*", SYMLINK+="disk/by-id/SERIAL_ATA-$env{ID_SERIAL}"
+KERNEL=="sd*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="partition",
+ENV{ID_SERIAL}=="?*",
+SYMLINK+="disk/by-id/SERIAL_ATA-$env{ID_SERIAL}-part%n"
+
+KERNEL=="sr*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="disk",
+ENV{ID_BUS}=="usb*", SYMLINK+="disk/by-id/USB-$env{ID_USB}"
+KERNEL=="sd*", ENV{ID_BUS}=="ata", ENV{DEVTYPE}=="partition",
+ENV{ID_SERIAL}=="?*",
+SYMLINK+="disk/by-id/USB-$env{ID_USB}-part%n"
 
 " >> /lib/udev/rules.d/60-persistent-storage.rules
