6fb523d61f6ac22b1ae67b647ee44be60984c542
Sharrisii <peregrinebrian@gmail.com>
2018-05-13 15:12:00 +0200

    Update other.sh

--- bootscripts/other.sh
+++ bootscripts/other.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Place any other commands you still need in this file. This may include copying settings for gajim, linphone, claws-mail, ...
 
+# Example code: gajim
+# First, start gajim and enter the configuration settings for your messenger account(s)
+# Next, copy your profile file (config.default) from ? to /mnt/*/profiles/gajim
+# The rest is handled by following code:
+cp /mnt/*/profiles/gajim/* ?
+gajim -p /?/config.default
+
 # Example code: claws-mail
 # First, start claws-mail and enter the configuration settings for your (POP3 or IMAP) account
 # Next, copy your configuration files from /home/username/.claws-mail to /mnt/*/emails/.clawsconfig/
