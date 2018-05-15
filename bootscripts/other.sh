da388d630d7c36e431f59fc1622908d61adcbe8d
Sharrisii <peregrinebrian@gmail.com>
2018-05-15 10:27:05 +0200

    Since there's no non-privileged user account, changing /home/username: paths to /

--- bootscripts/other.sh
+++ bootscripts/other.sh
@@ -3,25 +3,25 @@
 
 # Example code: gajim
 # First, start gajim and enter the configuration settings for your messenger account(s)
-# Next, rename your profile text to default.txt and copy it from /home/username/.config/gajim/ to /mnt/*/profiles/gajim
+# Next, rename your profile text to default.txt and copy it from /.config/gajim/ to /mnt/*/profiles/gajim 
 # The rest is handled by following code:
 gajim -p default -c /mnt/*/profiles/gajim/
 
 # Example code: linphone
 # First, start linphone and enter the configuration settings for your messenger account(s)
-# Next, copy your profile file from /home/username/.linphonerc to /mnt/*/profiles/linphone
+# Next, copy your profile file from /.linphonerc to /mnt/*/profiles/linphone 
 # See also http://write.flossmanuals.net/linphone/configuring/
 # The rest is handled by following code:
-cp /mnt/*/profiles/linphone/* /home/*/.linphonerc/
-
+cp /mnt/*/profiles/linphone/* /.linphonerc/ 
+ 
 # Example code: claws-mail
 # First, start claws-mail and enter the configuration settings for your (POP3 or IMAP) account
-# Next, copy your configuration files from /home/username/.claws-mail to /mnt/*/emails/.clawsconfig/
+# Next, copy your configuration files from /.claws-mail to /mnt/*/emails/.clawsconfig/
 # By configuration files I mean not just the user account data but also program data (important for instance to skip
 # the account registration box that auto-launches the first time (= first run)
 # The rest is handled by following code:
 if [ -f /usr/bin/claws-mail ] then
 claws-mail --alternate-config-dir /mnt/*/emails/.clawsconfig/
-cp -f /home/*/Mail/inbox/*.eml /mnt/*/emails/inbox/
-cp -f /home/*/Mail/draft/*.eml /mnt/*/emails/draft/
-cp -f /home/*/Mail/sent/*.eml /mnt/*/emails/sent/
+cp -f /Mail/inbox/*.eml /mnt/*/emails/inbox/
+cp -f /Mail/draft/*.eml /mnt/*/emails/draft/
+cp -f /Mail/sent/*.eml /mnt/*/emails/sent/
