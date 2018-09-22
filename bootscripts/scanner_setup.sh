f50f30c6f20bb185cc9f4729f25308b39901a53d
Sharrisii <peregrinebrian@gmail.com>
2018-09-22 13:39:03 +0200

    Update scanner_setup.sh

--- bootscripts/scanner_setup.sh
+++ bootscripts/scanner_setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 #See https://wiki.gentoo.org/wiki/SANE
 #and https://help.ubuntu.com/community/SANE%20-%20Installing%20a%20scanner%20that%20isn%27t%20auto-detected
+
+
 #Set use flag
 # ?
+
+# Check whether your scanner is supported by SANE by viewing http://www.sane-project.org/sane-supported-devices.html
+# If the scanner is supported, open the file /etc/sane.d/dll.conf
+# Remove the hashtag before your brand of scanner before "example-backend"
+
+
 sane-find-scanner
 
 #Add to user group
