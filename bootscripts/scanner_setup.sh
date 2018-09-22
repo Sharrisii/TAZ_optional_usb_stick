f2244f1b4bdeda5d9f739f06ff5c68443caf69a8
Sharrisii <peregrinebrian@gmail.com>
2018-09-22 09:40:02 +0200

    Update scanner_setup.sh

--- bootscripts/scanner_setup.sh
+++ bootscripts/scanner_setup.sh
@@ -3,6 +3,7 @@
 #and https://help.ubuntu.com/community/SANE%20-%20Installing%20a%20scanner%20that%20isn%27t%20auto-detected
 #Set use flag
 # ?
+sane-find-scanner
 
 #Add to user group
 usermod -aG scanner <username>
