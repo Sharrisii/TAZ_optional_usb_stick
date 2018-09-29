e4136f0c24bc3c77bcc43db69b699c2a44a21008
Sharrisii <peregrinebrian@gmail.com>
2018-09-29 17:37:44 +0200

    Update wallpaper.sh

--- bootscripts/wallpaper.sh
+++ bootscripts/wallpaper.sh
@@ -2,4 +2,4 @@
 # Commands in the /bootscripts folder have priority over the basic wallpaper configuration
 # So, altering these will change your the settings loaded by the TAZ livecd/usb
 
-xsetroot #68BEE8
+xsetroot #B7E4F1
