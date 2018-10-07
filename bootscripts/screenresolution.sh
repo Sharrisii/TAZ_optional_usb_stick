50b724e5dcfea80b27eb5272c5977effc33ab808
Sharrisii <peregrinebrian@gmail.com>
2018-10-07 15:49:01 +0200

    Update screenresolution.sh

--- bootscripts/screenresolution.sh
+++ bootscripts/screenresolution.sh
@@ -1,4 +1,3 @@
-# This file is triggered by the screen_res_lookup_on_usb_drives.start file in /etc/local.d/ in the TAZ distro (livecd/liveusb).
 # The basic screen resolution is first used from /etc/X11/xorg.conf.d/, after this, the command below is carried out
 # which allow setting a different resolution
 # see https://wiki.gentoo.org/wiki/Xrandr
