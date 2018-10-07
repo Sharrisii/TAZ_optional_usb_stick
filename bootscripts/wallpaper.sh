7940d1f0c0879eeaace0cd634a4b77cc97e474aa
Sharrisii <peregrinebrian@gmail.com>
2018-10-07 15:49:26 +0200

    Update wallpaper.sh

--- bootscripts/wallpaper.sh
+++ bootscripts/wallpaper.sh
@@ -1,4 +1,3 @@
-# This file is triggered by the wallpaper_lookup_on_usb_drives.start file in /etc/local.d/ in the TAZ distro (livecd/liveusb).
 # Commands in the /bootscripts folder have priority over the basic wallpaper configuration
 # So, altering these will change your the settings loaded by the TAZ livecd/usb
 
