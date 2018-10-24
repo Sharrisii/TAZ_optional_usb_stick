6d43964391767640b0f2c1130dc352955fdf76b5
Sharrisii <peregrinebrian@gmail.com>
2018-10-24 13:12:25 +0200

    Update other.sh

--- bootscripts/other.sh
+++ bootscripts/other.sh
@@ -30,9 +30,14 @@ cp -f /Mail/sent/*.eml /mnt/*/emails/sent/
 # Example code: gnupg public, private (and/or subkeys) import
 # See https://www.phildev.net/pgp/gpg_moving_keys.html
 # and http://irtfweb.ifa.hawaii.edu/~lockhart/gpg/
-cd  ../e-mails/gnupg_keys/
+# and https://www.gnupg.org/gph/en/manual/x56.html
+# and https://futureboy.us/pgp.html
+cd  ../e-mails/gnupg_keys/mykeys/
 gpg --import *.asc
 trust 5
+cd  ../e-mails/gnupg_keys/otherskeys/
+gpg --import *.asc
+trust 3
 
 # Example code: dhcpcd
 # Use this only if you need to set up your internet connection (ethernet modems generally don't need it, config of these are
