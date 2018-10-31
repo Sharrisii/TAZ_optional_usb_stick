573852d82d3daf077b9cb3b5850255549986d757
Sharrisii <peregrinebrian@gmail.com>
2018-10-31 14:45:10 +0100

    Update and rename wallpaper.sh to fonts_themes.sh

---
+++ bootscripts/fonts_themes.sh
@@ -0,0 +1,4 @@
+# Commands in the /bootscripts folder have priority over the basic fonts/themes configuration
+# So, altering these will change your the settings loaded by the TAZ livecd/usb
+
+cp -f
