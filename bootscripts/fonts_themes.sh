46748c846741b4bb7413dabaa02e5733dd0435c6
Sharrisii <peregrinebrian@gmail.com>
2018-10-31 14:58:21 +0100

    Update fonts_themes.sh

--- bootscripts/fonts_themes.sh
+++ bootscripts/fonts_themes.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Commands in the /bootscripts folder have priority over the basic fonts/themes configuration
 # So, altering these will change your the settings loaded by the TAZ livecd/usb
 
