1f237d3ed4b937d91ed4ce11c993fd58aa7c9761
Sharrisii <peregrinebrian@gmail.com>
2018-11-11 13:43:05 +0100

    Update fonts_themes.sh

--- bootscripts/fonts_themes.sh
+++ bootscripts/fonts_themes.sh
@@ -2,7 +2,7 @@
 # Commands in the /bootscripts folder have priority over the basic fonts/themes configuration
 # So, altering these will change your the settings loaded by the TAZ livecd/usb
 
-# cp -f ../fonts_themes/settings.ini /home/taz/.config/gtk-3.0/
-# cp -f ../fonts_themes/leafpadrc /home/taz/.config/leafpad/
-# cp -f ../fonts_themes/pcmanfm.conf /home/taz/.config/pcmanfm/LXDE/
-# cp -f ../fonts_themes/desktop-items-0.conf /home/taz/.config/pcmanfm/LXDE/
+# cp -f /mnt/*/fonts_themes/settings.ini /home/taz/.config/gtk-3.0/
+# cp -f /mnt/*/fonts_themes/leafpadrc /home/taz/.config/leafpad/
+# cp -f /mnt/*/fonts_themes/pcmanfm.conf /home/taz/.config/pcmanfm/LXDE/
+# cp -f /mnt/*/fonts_themes/desktop-items-0.conf /home/taz/.config/pcmanfm/LXDE/
