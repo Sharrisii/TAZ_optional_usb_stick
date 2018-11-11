39119bf36bffcf4e56b5f4ccef45469f0be5d35a
Sharrisii <peregrinebrian@gmail.com>
2018-11-11 13:49:02 +0100

    Update scanner_setup.sh

--- bootscripts/scanner_setup.sh
+++ bootscripts/scanner_setup.sh
@@ -5,14 +5,15 @@
 
 # Check whether your scanner is supported by SANE by viewing http://www.sane-project.org/sane-supported-devices.html
 # If the scanner is supported, this script will add the user taz and root to the scanner user group
-usermod -aG scanner taz
-usermod -aG scanner root
+# Since in TAZ, both taz and root are already part of the scanner group, this section is outcommented
+# usermod -aG scanner taz
+# usermod -aG scanner root
 
 # If it's a usb scanner, also open up /etc/sane.d/example.conf and find the line with the word "usb" in it and then add 
 # the vendor and the product number you got with the scanimage -L command. It will look similar like this: 0x01aa 0x0001
 # Copy this example.conf to the usb stick folder /miscellaneous/ and have this script run the command below by outcommenting 
 # the following line in this script:
-# cp -f ../miscellaneous/example.conf /etc/sane.d/example.conf
+# cp -f /mnt/*/miscellaneous/example.conf /etc/sane.d/example.conf
 # Remember: only do this if it's a usb scanner.
 
 # If the scanner hasn't been detected (check with scanimage -L or sane-find-scanner), then open the file /etc/sane.d/dll.conf and 
@@ -20,4 +21,4 @@ usermod -aG scanner root
 
 # Then, copy the dll.conf to the usb stick folder /miscellaneous/ and have this script run the commands 
 # below by outcommenting the following line in this script:
-# cp -f ../miscellaneous/dll.conf /etc/sane.d/dll.conf
+# cp -f /mnt/*/miscellaneous/dll.conf /etc/sane.d/dll.conf
