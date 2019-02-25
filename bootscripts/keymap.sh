7847e4b57c3346e976858087a3831946124791de
Sharrisii <peregrinebrian@gmail.com>
2019-02-25 15:18:21 +0000

    Update keymap.sh

--- bootscripts/keymap.sh
+++ bootscripts/keymap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# this file overrides the 30-keyboard.conf file located in /etc/X11/xorg.conf.d; if the usb stick is not inserted 
-# (and this file is hence unavailable, the keymap in /etc/X11/xorg.conf.d/30-keyboard.conf will be used instead.
+# this file overrides either keymap selection (latest versions of TAZ) or the 10-keyboard.conf file located in /etc/X11/xorg.conf.d;
+# present in older versions of TAZ, if the usb stick is not inserted; it will then override that file.
 # see https://wiki.gentoo.org/wiki/Xorg/Guide#Configuring_the_keyboard
 # and https://wiki.gentoo.org/wiki/Keyboard_layout_switching#Using_setxkbmap
+# To see your current set keymap (in X), use the command "xkeyboard-config"
 
 # the command below sets the belgian latin1 version of the azerty keyboard;
 # see /usr/share/kbd/keymaps/i386/azerty
