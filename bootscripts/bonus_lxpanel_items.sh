180980aac5e7735d0b6b0d961c9f33587d2ea37c
Default LiveCD User <taz@livecd.gentoo>
2019-04-17 17:07:53 +0000

    update

--- bootscripts/bonus_lxpanel_items.sh
+++ bootscripts/bonus_lxpanel_items.sh
@@ -3,16 +3,5 @@
 
 # Add items when launching boot setup 5
 if [ -f /usr/bin/gimp/ ]; 
-then
-copy file with info below to /usr/share/applications:
-[Desktop Entry]
-Type=Application
-Icon=leafpad
-Name=Scan-ocr
-Comment=Scan a page (OCR) and load text into leafpad
-Categories=Graphics;2DGraphics;RasterGraphics;GTK;
-Exec= /mnt/*/additional_scripts/scan-ocr.sh
-StartupNotify=true
-Terminal=true
-MimeType=inode/directory;
-fi
+then cp -f ../desktop_files/Scan-ocr.desktop /usr/share/applications
+done
