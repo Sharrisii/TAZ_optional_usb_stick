fb0fe51780e43be24198b439fb8d6b9ab7de259d
Sharrisii <peregrinebrian@gmail.com>
2019-08-17 12:01:04 +0200

    Update live_usb_creator.sh

--- miscellaneous/live_usb_creator.sh
+++ miscellaneous/live_usb_creator.sh
@@ -3,6 +3,6 @@
 # it is not intented for the optional_usb_stick as this can be simply copied unto a (formatted) usb stick/memory card
 # See https://wiki.gentoo.org/wiki/LiveUSB/Guide
 # Change the path of /dev/sdc to the path of your usb stick/memory card;
-# make sure the path is correct, to prevent accidental formatting of other connected drives
+# Make sure the path is correct, to prevent accidental formatting of other connected drives
 
 dd if=/path/to/image.iso of=/dev/sdc bs=8192k
