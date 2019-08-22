c567628a6469f3b192a3e4b7a52ecaf766127ac1
Sharrisii <peregrinebrian@gmail.com>
2019-08-22 13:31:39 +0200

    Update live_usb_creator.sh

--- miscellaneous/live_usb_creator.sh
+++ miscellaneous/live_usb_creator.sh
@@ -6,3 +6,9 @@
 # Make sure the path is correct, to prevent accidental formatting of other connected drives
 
 dd if=/path/to/image.iso of=/dev/sdc bs=8192k
+
+# Uncomment the line below to make swap space on the usb stick (to increase your memory capacity; 
+# especially useful if your machine lacks in this and you also don't have a regular hard drive in your machine)
+# To change the amount of swapspace (1000mb in the command), simply change the number.
+# The swapspace in activated at boot by another command at /bootscripts/other.sh 
+#mkfileswap /mnt/sdb2\swapspace_for_TAZ.swap 1000
